b4be4b0518ba082a5d42549cd4442241fdc776d7
peter88213 <peter.88213@t-online.de>
2022-01-17 14:43:34 +0100

    Update docstrings.

---
+++ src/ywrenumber/__init__.py
@@ -0,0 +1,11 @@
+"""Package for yWriter chapter renumbering.
+
+Modules:
+
+yw_rn -- Provide a report generator class for yWriter projects.
+rn_ui -- Provide a Tkinter facade user interface class.
+
+Copyright (c) 2022 Peter Triesberger
+For further information see https://github.com/peter88213/yw-reporter
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
