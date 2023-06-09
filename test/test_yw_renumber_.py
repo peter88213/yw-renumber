94ad135a1e5b55c6dc3555bfb31718dcc8a40273
peter88213 <peter.88213@t-online.de>
2023-06-09 09:35:20 +0200

    Update docstrings

--- test/test_yw_renumber_.py
+++ test/test_yw_renumber_.py
@@ -1,7 +1,6 @@
-""" Regression test for the yw_renumber project.
+"""Regression test for the yw_renumber project.
 
-Test suite for yw_renumber.pyw.
-
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/yw-renumber
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
